d14e5f36b1a0e7129eaf5540379f6f47da716ef6
raunol <3lillr84@solent.ac.uk>
2018-12-14 00:30:47 +0200

    VUE template

---
+++ website/src/main.js
@@ -0,0 +1,23 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import './sass/main.scss'
+import './sass/layout/_grid.scss'
+import './sass/layout/_header.scss'
+import './sass/layout/_navigation.scss'
+import './sass/abstracts/_variables.scss'
+import './sass/base/_typography.scss'
+import './sass/pages/_index.scss'
+import './sass/components/_buttons.scss'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
