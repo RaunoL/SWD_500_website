43c37c89c272431adf494d26f15769b21605300d
RaunoL <raunolillemets@hotmail.com>
2018-12-14 00:56:53 +0200

    Hero and intro components

---
+++ website/src/components/hero.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "hero"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- website/src/router/index.js
+++ website/src/router/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import hero from '@/components/hero'
 
-Vue.use(Router)
+
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'hero',
+      component: hero
     }
   ]
 })
